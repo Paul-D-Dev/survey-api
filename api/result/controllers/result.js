b686a996d25daaa86efb2a6dc27d238dd0a33162
Paul DAO <dao.van.paul@gmail.com>
2020-11-05 11:36:14 +0100

    Delete DATA table and custom create method result and add create from response inside

--- api/result/controllers/result.js
+++ api/result/controllers/result.js
@@ -1,8 +1,22 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.result.create(data, { files });
+    } else {
+      // Save to Result table then to Response to display the pies
+      entity = await strapi.services.result.create(ctx.request.body);
+      await strapi.services.response.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.result });
+  },
+};
