d589b0f1c570e5175d317b4e273e655459dbf2fd
Paul DAO <dao.van.paul@gmail.com>
2020-11-16 15:59:35 +0100

    Update database config

--- config/database.js
+++ config/database.js
@@ -4,8 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        // client: 'sqlite',
+        // filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'sql',
+        host: "${process.env.HOST}",
+        port: "${process.env.PORT}",
+        database: "${process.env.DATABASE}",
+        username: "${process.env.USERNAME}",
+        password: "${process.env.PASSWORD}",
+        ssl: { rejectUnauthorized: false }
       },
       options: {
         useNullAsDefault: true,
